e84fe26e034069b0155867cf1b29d6d155078397
hywznn <choihj0510@naver.com>
2025-10-10 02:21:00 +0900

    chore(#4): 업데이트 husky hooks 및 commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,6 +6,10 @@ module.exports = {
       'always',
       ['feat', 'fix', 'refactor', 'chore', 'docs', 'perf', 'test', 'ci', 'build', 'revert'],
     ],
+    'scope-case': [2, 'always', 'lower-case'],
+    'scope-empty': [2, 'never'], // Scope is required
+    'scope-min-length': [2, 'always', 2], // Minimum length for scope
+    'scope-max-length': [2, 'always', 20], // Maximum length for scope
     'subject-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
